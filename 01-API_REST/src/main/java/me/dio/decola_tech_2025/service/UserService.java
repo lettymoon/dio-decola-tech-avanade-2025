2f82aa7347a8c86c4d9f97c17148d02a6a30a29a
Leticia <lettinwonderland@gmail.com>
2025-03-03 21:26:41 -0300

    feat: camada de negócio

---
+++ 01-API_REST/src/main/java/me/dio/decola_tech_2025/service/UserService.java
@@ -0,0 +1,10 @@
+package me.dio.decola_tech_2025.service;
+
+import me.dio.decola_tech_2025.domain.model.User;
+
+public interface UserService {
+
+    User findById(Long id);
+
+    User create(User userToCreate);
+}
