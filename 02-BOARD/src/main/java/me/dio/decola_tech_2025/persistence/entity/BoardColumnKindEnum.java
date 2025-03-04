0764f25115cdebebdcf1cfc0beee6bccb519ff45
Leticia <lettinwonderland@gmail.com>
2025-03-04 12:58:00 -0300

    feat: UI e acesso a dados

--- 02-BOARD/src/main/java/me/dio/decola_tech_2025/persistence/entity/BoardColumnKindEnum.java
+++ 02-BOARD/src/main/java/me/dio/decola_tech_2025/persistence/entity/BoardColumnKindEnum.java
@@ -1,5 +1,13 @@
 package me.dio.decola_tech_2025.persistence.entity;
 
+import java.util.stream.Stream;
+
 public enum BoardColumnKindEnum {
-    INITIAL, FINAL, CANCEL, PENDING
+    INITIAL, FINAL, CANCEL, PENDING;
+
+    public static BoardColumnKindEnum findByName(final String name) {
+        return Stream.of(BoardColumnKindEnum.values())
+                .filter(b -> b.name().equals(name))
+                .findFirst().orElseThrow();
+    }
 }
--- 02-BOARD/src/main/java/me/dio/decola_tech_2025/persistence/entity/BoardEntity.java
+++ 02-BOARD/src/main/java/me/dio/decola_tech_2025/persistence/entity/BoardEntity.java
@@ -1,6 +1,7 @@
 package me.dio.decola_tech_2025.persistence.entity;
 
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.List;
 public class BoardEntity {
     private Long id;
     private String name;
+    @ToString.Exclude
     private List<BoardColumnEntity> boardColumns = new ArrayList<>();
 }
